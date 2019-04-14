b4366cefa0c514ea24fa94fb7972f75633725559
ZhuangJiNan <zhuangjinan2018@gmail.com>
2019-04-14 21:33:09 +0800

    flatButton -i-u

---
+++ flatButton/main.js
@@ -0,0 +1,17 @@
+zButton.onfocus = function (e) {
+    zButton.classList.add('active')
+}
+zButton.onblur = function (e) {
+    zButton.classList.remove('active')
+}
+zButton.onclick = function(e){
+    let mask = document.createElement('span')
+    mask.style.left = e.offsetX + 'px';
+    mask.style.top = e.offsetY + 'px';
+    mask.id = 'mask'
+    mask.className = 'mask'
+    mask.addEventListener('animationend',function(e){
+        e.target.remove()
+    })
+    e.target.append(mask)
+}
