cd69d26863691f76c0cf107312e8b187aae4173b
Zhuangjinan <a492073467@gmail.com>
2019-05-15 20:22:36 +0800

    无缝轮播新方案

---
+++ swiper2/main.js
@@ -0,0 +1,90 @@
+let $buttons = $("#buttonWrapper>button");
+let $slides = $("#slides");
+let $images = $slides.children();
+let current = 0;
+makeFakeImage();
+bindEvents();
+
+let timer = setInterval(() => {
+    goToSlide(current + 1);
+}, 2000);
+
+function makeFakeImage() {
+    let $firstCopy = $images.eq(0).clone(true);
+    let $lastCopy = $images.eq($images.length - 1).clone(true);
+
+    $slides.append($firstCopy);
+    $slides.prepend($lastCopy);
+}
+function bindEvents() {
+    $buttons.on("click", e => {
+        let $button = $(e.currentTarget);
+        let index = $button.index();
+        goToSlide(index);
+    });
+    $(next).on("click", () => {
+        goToSlide(current + 1);
+    });
+    $(previous).on("click", () => {
+        goToSlide(current - 1);
+    });
+    $(".container").on("mouseenter", () => {
+        window.clearInterval(timer);
+    });
+    $(".container").on("mouseleave", () => {
+        timer = setInterval(() => {
+            goToSlide(current + 1);
+        }, 2000);
+    });
+    $(document).on("visibilitychange", () => {
+        if (document.visibilityState === "hidden") {
+            window.clearInterval(timer);
+        }
+
+        // 用户打开或回到页面
+        if (document.visibilityState === "visible") {
+            timer = setInterval(() => {
+                goToSlide(current + 1);
+            }, 2000);
+        }
+    });
+}
+
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0;
+    } else if (index < 0) {
+        index = $buttons.length - 1;
+    }
+
+    if (current === $buttons.length - 1 && index === 0) {
+        //最后一张到第一张
+        $slides
+            .css({
+                transform: `translateX(${-($buttons.length + 1) * 400}px)`
+            })
+            .one("transitionend", () => {
+                $slides.hide().offset();
+                $slides
+                    .css({
+                        transform: `translateX(${-(index + 1) * 400}px)`
+                    })
+                    .show();
+            });
+    } else if (current === 0 && index === $buttons.length - 1) {
+        //第一张到最后一张
+        $slides
+            .css({ transform: `translateX(0px)` })
+            .one("transitionend", () => {
+                $slides.hide().offset();
+                $slides
+                    .css({
+                        transform: `translateX(${-(index + 1) * 400}px)`
+                    })
+                    .show();
+            });
+    } else {
+        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` });
+    }
+    current = index;
+}
